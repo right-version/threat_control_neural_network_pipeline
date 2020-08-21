1bd964b18672a7445afdfff8d921b886c4a76707
vpvpvpvp <vktrpnzrv@gmail.com>
2020-08-22 00:15:14 +0300

    Default configuration

--- train/train.py
+++ train/train.py
@@ -1,12 +1,18 @@
 import torch
 import argparse
 from data.traffic_data_generator import TrafficDataset
+import yaml
+
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Neural network train script")
     parser.add_argument('--config', required=False, type=str,
-                        default='../config/default_train_config.yml',
+                        default='../configs/default_train_config.yml',
                         help='Path to configuration yml file.'
                         )
     return parser.parse_args()
@@ -17,6 +23,9 @@ def main():
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print("device: {}".format(device))
+
+    with open(args.config, "r") as f:
+        config = yaml.safe_load(f)
 
 
 if __name__ == '__main__':
